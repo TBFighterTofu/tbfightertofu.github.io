8311346920bc6870715ac9e2cb718b082b115b43
TBFighter Tofu <leonmuss4@gmail.com>
2025-02-12 22:57:17 -0800

    add scrapers

---
+++ p4a/src/read_views.py
@@ -0,0 +1,76 @@
+import requests
+import json
+import time
+import pandas as pd
+from pathlib import Path
+import os
+
+data_folder = Path(__file__).parent.parent / "data"
+video_file = data_folder / "videos.csv"
+all_views_file = data_folder / "all_views.json"
+views_json = data_folder / "views.json"
+
+def import_video_list():
+    v = pd.read_csv(video_file)
+    v = v[v.Year==2025]
+    v.reset_index(inplace=True, drop=True)
+    return v
+
+def scrape_view_chunk(vid_df: pd.DataFrame, timestamp) -> list[dict]:
+    out = []
+    API_KEY = os.environ.get('YT_TOKEN')
+    VIDEO_ID = ",".join([s.split("?v=")[1] for s in vid_df.url])
+    PARTS = 'statistics'
+    url = f'https://www.googleapis.com/youtube/v3/videos?part={PARTS}&id={VIDEO_ID}&key={API_KEY}'
+    response = requests.get(url).json()
+    with open('video_test_data.json', 'w', encoding='utf-8') as f:
+        json.dump(response, f, ensure_ascii=False, indent=4)
+    vids_data = response["items"]
+    for i,o in enumerate(vids_data):
+        oo = {}
+        oo["Charity"] = vid_df.iloc[i].Charity
+        oo["Timestamp"] = timestamp
+        oo["id"] = o["id"]
+        stats = o["statistics"]
+        oo = {**oo, **stats}
+        out.append(oo)
+    return out
+
+def add_view_summary(new_dfs: pd.DataFrame):
+    with open(all_views_file, "r") as f:
+        d = json.load(f)
+    d.append({"Timestamp":int(time.time()),
+              "viewCount":int(pd.to_numeric(new_dfs["viewCount"]).sum()),
+              "likeCount":int(pd.to_numeric(new_dfs["likeCount"]).sum()),
+              "favoriteCount":int(pd.to_numeric(new_dfs["favoriteCount"]).sum()),
+              "commentCount":int(pd.to_numeric(new_dfs["commentCount"]).sum()),
+              })
+    with open(all_views_file, "w") as f:
+        json.dump(d, f)
+    print(time.time(), all_views_file)
+
+def scrape_views():
+    views_df = pd.read_json(views_json)
+    vid_df = import_video_list()
+    dfs = []
+    i = 0
+    timestamp = time.time()
+    while i*50 < len(vid_df):
+        df = scrape_view_chunk(vid_df[50*i:min(50*(i+1), len(vid_df)-1)], timestamp)
+        dfs = dfs + df
+        i+=1
+    new_dfs = pd.DataFrame(dfs)
+    views_df = pd.concat([views_df, new_dfs])
+    views_df.reset_index(inplace=True, drop=True)
+    views_df.to_json(views_json, orient="records")
+    print(timestamp, views_json)
+    add_view_summary(new_dfs)
+    return views_df
+
+if __name__=="__main__":
+    while True:
+        try:
+            scrape_views()
+        except:
+            pass
+        time.sleep(60*15)
--- p4a/src/read_website.py
+++ p4a/src/read_website.py
@@ -3,16 +3,22 @@ import json
 import time
 import pandas as pd
 from pathlib import Path
+from datetime import datetime
+
+CURRENT_YEAR = str(datetime.now().year)
 
 data_folder = Path(__file__).parent.parent / "data"
-vote_count_file = data_folder / "vote_count.csv"
-features_file = data_folder / "features_2025.csv"
+vote_count_json = data_folder / "vote_count.json"
+features_json = data_folder / "feature_times.json"
+videos_file = data_folder / "videos.json"
+feature_list_file = data_folder / "features.json"
 
-video_file = data_folder / "videos.csv"
+video_csv = data_folder / "videos.csv"
 views_file = data_folder / "views.csv"
 
 def import_vote_count() -> pd.DataFrame:
-    f0 = pd.read_csv(vote_count_file)
+    # f0 = pd.read_csv(vote_count_file)
+    f0 = pd.read_json(vote_count_json)
     return f0
 
 def read_votes():
@@ -28,13 +34,17 @@ def read_votes():
 def refresh_votes():
     rv = read_votes()
     if "votes" in rv:
+        rv["votes"] = float(rv["votes"])
+        rv["total"] = float(rv["total"])
+        rv["donations"] = float(rv["donations"])
         try:
             f0 = import_vote_count()
             f0 = pd.concat([f0, pd.DataFrame([rv])])
         except pd.errors.EmptyDataError:
             f0 = pd.DataFrame([rv])
-        f0.to_csv(vote_count_file, index=False)
-        print(time.time(), vote_count_file)
+        f0.reset_index(drop=True, inplace=True)
+        f0.to_json(vote_count_json, orient="records")
+        print(time.time(), vote_count_json)
 
 def read_featured():
     url = "https://projectforawesome.com/featured"
@@ -45,19 +55,45 @@ def read_featured():
     except:
         return {}
 
-def import_features() -> pd.DataFrame:
-    f0 = pd.read_csv(features_file)
-    return f0
+def import_features_2() -> dict:
+    with open(features_json, "r") as f:
+        d = json.load(f)
+    return d
 
-    
+def export_features_2(d) -> dict:
+    print(time.time(), features_json)
+    with open(features_json, "w") as f:
+        json.dump(d, f)
+
+def export_feature_list(id: str):
+    with open(feature_list_file, "r") as f:
+        fd = json.load(f)
+    vf = pd.read_csv(video_csv)
+    yf = f"https://www.youtube.com/watch?v={id}"
+    row = vf[vf.url==yf]
+    if len(row)>0:
+        charity = row.iloc[0]["Charity"]
+        if charity not in fd:
+            fd[charity] = [CURRENT_YEAR]
+        if CURRENT_YEAR not in fd[charity]:
+            fd[charity].append(CURRENT_YEAR)
+    with open(feature_list_file, "w") as f:
+        json.dump(fd, f)
+    print(time.time(), feature_list_file)
+
 def refresh_features():
     rv = read_featured()
     if "video" in rv:
-        f0 = import_features()
-        if f0.iloc[-1]["video"]!=rv["video"]:
-            f0 = pd.concat([f0, pd.DataFrame([rv])])
-            f0.to_csv(features_file, index=False)
-            print(time.time(), features_file)
+        f1 = import_features_2()
+        id = rv["video"].replace("https://projectforawesome.com/watch?v=", "")
+        if CURRENT_YEAR not in f1:
+            f1[CURRENT_YEAR] = {}
+        if id in f1[CURRENT_YEAR]:
+            f1[CURRENT_YEAR][id]["end"] = time.time()
+        else:
+            f1[CURRENT_YEAR][id] = {"start":time.time(), "end":time.time()}
+            export_feature_list(id)
+        export_features_2(f1)
 
 if __name__=="__main__":
     while True:
