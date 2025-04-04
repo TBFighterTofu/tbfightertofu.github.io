a15cf0ec23553a214e7ebd7c71268b9e48b1ad28
TBFighter Tofu <leonmuss4@gmail.com>
2025-04-03 20:32:32 -0700

    add latent cases

--- hill_day/src/convert_cases.py
+++ hill_day/src/convert_cases.py
@@ -11,9 +11,14 @@ def to_float(s:str) -> float:
 def floats(s: pd.Series) -> list[float]:
     return [to_float(x) for x in s]
 
+DATAFOLDER = Path(__file__).parent.parent / "data"
+
+with open(DATAFOLDER / "state_abbrevs.json") as f:
+    ABBREVS = json.load(f)
+
 def convert():
-    df1 = pd.read_csv(Path(__file__).parent.parent / "data/AtlasPlusTableData.csv")
-    df2 = pd.read_csv(Path(__file__).parent.parent / "data/2024_cases.csv")
+    df1 = pd.read_csv(DATAFOLDER / "AtlasPlusTableData.csv")
+    df2 = pd.read_csv(DATAFOLDER / "2024_cases.csv")
     df2["Year"] = ["2024" for _ in range(len(df2))]
     df = pd.concat([df1[["Year", "Geography", "Cases", "Rate per 100000"]], df2])
     geographies = df.Geography.unique()
@@ -25,7 +30,16 @@ def convert():
         out[geography] = {"Year":floats(df3.Year), 
                           "Cases":floats(df3.Cases), 
                           "Rate per 100000":floats(df3["Rate per 100000"])}
-    with open(Path(__file__).parent.parent / "data" / "cases" / f"cases.json", "w") as f:
+    df4 = pd.read_csv(DATAFOLDER / "latent.csv")
+    df4 = df4[df4.Grouping == "Total estimated number of persons with LTBI"]
+    for i,row in df4.iterrows():
+        state = row.Area.strip() 
+        if state in ABBREVS:
+            out[ABBREVS[state]]["latent_cases"] = row["Persons with LTBI (95%CI)"].split(" (")[0].strip()
+            out[ABBREVS[state]]["latent_pct"] = row["LTBI % (95%CI)"].split(" (")[0].strip()
+        else:
+            print(state, ABBREVS)
+    with open(DATAFOLDER / "cases" / f"cases.json", "w") as f:
         json.dump(out, f)
 
 if __name__=="__main__":
